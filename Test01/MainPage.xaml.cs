61f990cf7129a1fc95b4f0a92ba022a2c46db6b7
José Alejandro Sánchez Ortega <jasanchezo@gmail.com>
2018-12-17 15:13:09 -0600

    Inserción inicial en el repositorio del módulo Test01

---
+++ Test01/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Test01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            RuntimeImage.Source = ImageSource.FromUri(new Uri("https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"));
+
+            // throw new NotImplementedException();
+        }
+    }
+}
---
+++ Test01/ViewModel.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Test01
+{
+    public class ViewModel
+    {
+        public int ID { get; set; }
+    }
+}
